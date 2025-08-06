a48c71610ecb87fdecc7561be5898045e3f7555b
javier25arroyo <javier25arojas@gmail.com>
2025-08-05 20:18:55 -0600

    Update GameSeeder.java

--- src/main/java/com/project/demo/logic/entity/game/GameSeeder.java
+++ src/main/java/com/project/demo/logic/entity/game/GameSeeder.java
@@ -4,6 +4,9 @@ import com.project.demo.logic.entity.game.repository.GameRepository;
 import com.project.demo.logic.entity.settings.LevelEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,16 +15,22 @@ import org.springframework.stereotype.Component;
  * para todos los tipos de juego disponibles.
  */
 @Component
-public class GameSeeder implements CommandLineRunner {
+@Order(3)
+@org.springframework.context.annotation.Profile("!test")
+public class GameSeeder implements ApplicationListener<ContextRefreshedEvent> {
 
     @Autowired
     private GameRepository gameRepository;
 
+    /**
+     * Método que se ejecuta al iniciar la aplicación.
+     * Este método se encarga de sembrar los juegos iniciales en la base de datos
+     * @param contextRefreshedEvent
+     */
+
     @Override
-    public void run(String... args) throws Exception {
-        if (gameRepository.count() == 0) {
-            seedGames();
-        }
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        this.seedGames();
     }
 
     /**
