b121b2e1445c6f712377ec783fb0422e08ca7e24
Luisferha <luisferha04@gmail.com>
2025-08-09 01:39:01 -0600

    clases eliminadas sin uso

--- src/main/java/com/project/demo/logic/entity/game/repository/GameComponentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.demo.logic.entity.game.repository;
-
-/**
- * Repositorio para la entidad {@link com.project.demo.logic.entity.game.GameComponent}.
- * Proporciona métodos para realizar operaciones CRUD sobre los componentes de juegos.
- * (Actualmente sin implementar métodos específicos)
- */
-public interface GameComponentRepository {
-}
